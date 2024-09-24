bb408fb22c4465b8e618b2aa8736ab344fb806e5
ZionYin <yzyrmb8@outlook.com>
2024-09-24 16:10:38 -0400

    Make sign-in page prettier

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,5 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { NextResponse } from 'next/server'
 
 const isProtectedRoute = createRouteMatcher([
     "/",
@@ -9,6 +10,8 @@ export default clerkMiddleware((auth, req) => {
     if (isProtectedRoute(req)) {
         auth().protect()
     }
+
+    return NextResponse.next()
 })
 
 export const config = {
