3ce7dbe83692ec48b67d7a159aaafed15f380e51
ZionYin <yzyrmb8@outlook.com>
2024-10-27 12:30:41 -0400

    Use rpc instead of mock data for accounts

--- app/(dashborad)/accounts/page.tsx
+++ app/(dashborad)/accounts/page.tsx
@@ -1,41 +1,17 @@
 "use client";
 
+import { DataTable } from "@/components/data-table";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { useGetAccounts } from "@/features/accounts/api/use-get-accounts";
 import { useNewAccount } from "@/features/accounts/hooks/use-new-accounts";
 import { Plus } from "lucide-react";
-import { columns, Payment } from "./columns";
-import { DataTable } from "@/components/data-table";
+import { columns } from "./columns";
 
 const AccountsPage = () => {
   const newAccount = useNewAccount();
-
-  const data: Payment[] = [
-    {
-      id: "728ed52f",
-      amount: 100,
-      status: "pending",
-      email: "m@example.com",
-    },
-    {
-      id: "728ed52g",
-      amount: 200,
-      status: "processing",
-      email: "j@example.com",
-    },
-    {
-      id: "728ed52h",
-      amount: 300,
-      status: "success",
-      email: "b@example.com",
-    },
-    {
-      id: "728ed52i",
-      amount: 400,
-      status: "failed",
-      email: "l@example.com",
-    },
-  ];
+  const accountsQuery = useGetAccounts();
+  const accounts = accountsQuery.data || [];
 
   return (
     <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
@@ -50,7 +26,7 @@ const AccountsPage = () => {
         <CardContent>
           <DataTable
             columns={columns}
-            data={data}
+            data={accounts}
             filterKey="email"
             onDelete={() => {}}
             disabled={false}          />
