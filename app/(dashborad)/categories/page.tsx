2865e1fb0ceb93450c498b23ce3af903a95582dc
ZionYin <yzyrmb8@outlook.com>
2024-10-28 14:00:57 -0400

    Add category page, api, hook

---
+++ app/(dashborad)/categories/page.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import { DataTable } from "@/components/data-table";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Skeleton } from "@/components/ui/skeleton";
+import { useBulkDeleteCategories } from "@/features/categories/api/use-bulk-delete";
+import { useGetCategories } from "@/features/categories/api/use-get-categories";
+import { useNewCategory } from "@/features/categories/hooks/use-new-category";
+import { Loader2, Plus } from "lucide-react";
+import { columns } from "./columns";
+
+const CategoriesPage = () => {
+  const newCategory = useNewCategory();
+  const categoriesQuery = useGetCategories();
+  const deleteCategories = useBulkDeleteCategories();
+  const categories = categoriesQuery.data || [];
+
+  const isDisabled = categoriesQuery.isLoading || deleteCategories.isPending;
+
+  if (categoriesQuery.isLoading) {
+    return (
+      <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
+        <Card className="border-none drop-shadow-sm">
+          <CardHeader className="gap-y-2 flex-row items-center justify-between">
+            <CardTitle className="text-xl line-clamp-1">Categories page</CardTitle>
+              <Skeleton className="h-8 w-48"/>
+          </CardHeader>
+          <CardContent>
+            <div className="h-[500px] w-full flex items-center justify-center">
+              <Loader2 className="animate-spin text-slate-300 size-6" />
+            </div>
+          </CardContent>
+        </Card>
+      </div>
+    );
+  }
+
+  return (
+    <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
+      <Card className="border-none drop-shadow-sm">
+        <CardHeader className="gap-y-2 flex-row items-center justify-between">
+          <CardTitle className="text-xl line-clamp-1">Categories page</CardTitle>
+          <Button size="sm" onClick={newCategory.onOpen}>
+            <Plus className="size-4 mr-r" />
+            Add new
+          </Button>
+        </CardHeader>
+        <CardContent>
+          <DataTable
+            columns={columns}
+            data={categories}
+            filterKey="name"
+            onDelete={(row) => {
+              const ids = row.map((row) => row.original.id);
+              deleteCategories.mutate({ids});
+            }}
+            disabled={isDisabled}          />
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
+
+export default CategoriesPage;
+
