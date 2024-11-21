589c08eb5c21a7ff454d4c641162521a1ff3ce94
ZionYin <yzyrmb8@outlook.com>
2024-11-21 17:59:22 -0500

    Add progress for continue when selecting headers for csv import

--- app/(dashborad)/transactions/import-card.tsx
+++ app/(dashborad)/transactions/import-card.tsx
@@ -2,6 +2,8 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { useState } from "react";
 import { ImportTable } from "./import-table";
+import { convertAmountToMiliunits } from "@/lib/utils";
+import { format, parse } from "date-fns";
 
 type Props = {
   data: string[][];
@@ -49,6 +51,51 @@ export const ImportCard = ({ data, onCancel, onSubmit }: Props) => {
     });
   };
 
+  const progress = Object.values(selectedColumns).filter(Boolean).length;
+
+  const handleContinue = () => {
+    const getColumnIndex = (column: string) => {
+      return column.split("_")[1];
+    };
+
+    const mappedData = {
+      heades: headers.map((_header, index) => {
+        const columnIndex = getColumnIndex(`column_${index}`);
+        return selectedColumns[`column_${index}`] || null;
+      }),
+      body: body
+        .map((row) => {
+          const transformedRow = row.map((cell, index) => {
+            const columnIndex = getColumnIndex(`column_${index}`);
+            return selectedColumns[`column_${index}`] ? cell : null;
+          });
+
+          return transformedRow.every((item) => item === null)
+            ? []
+            : transformedRow;
+        })
+        .filter((row) => row.length > 0),
+    };
+
+    const arrayOfData = mappedData.body.map((row) => {
+      return row.reduce((acc: any, cell, index) => {
+        const header = mappedData.heades[index];
+        if (header !== null) {
+          acc[header] = cell;
+        }
+        return acc;
+      }, {});
+    });
+
+    const formattedData = arrayOfData.map((item) => ({
+      ...item,
+      amount: convertAmountToMiliunits(parseFloat(item.amount)),
+      date: format(parse(item.date, dataFormat, new Date()), outputFormat),
+    }));
+
+    onSubmit(formattedData);
+  };
+
   return (
     <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
       <Card className="border-none drop-shadow-sm">
@@ -59,6 +106,13 @@ export const ImportCard = ({ data, onCancel, onSubmit }: Props) => {
           <div className="flex items-center gap-x-2">
             <Button size="sm" onClick={onCancel}>
               Cancel
+            </Button>
+            <Button
+              size="sm"
+              disabled={progress < requireOptions.length}
+              onClick={handleContinue}
+            >
+              Continue ({progress} / {requireOptions.length})
             </Button>
           </div>
         </CardHeader>
